c7a3063e33d7c83f88ec715fa324a943c1869116
jfaquinopando <pandolcik@gmail.com>
2023-04-12 21:20:16 -0400

    Generar campos desde js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+const campos = [
+    {
+        texto: "nombre",
+        tipo: "text"
+    },
+    {
+        texto: "apellido",
+        tipo: "text"
+    },
+    {
+        texto: "fecha_nac",
+        tipo: "date"
+    },
+    {
+        texto: "email",
+        tipo: "email"
+    },
+    {
+        texto: "tipo_documento",
+        tipo: "text"
+    },
+    {
+        texto: "numero_documento",
+        tipo: "text"
+    },
+]
+
+const test = campos.map(function (elemento, indice) {
+    return `<div class="form-group">
+                <label for="${elemento.texto}">${elemento.texto}</label>
+                <input class="form-control" type="${elemento.tipo}" name="${elemento.texto}" id="${elemento.texto}" placeholder="${elemento.texto}">
+            </div>`
+})
+
+console.log("veamos el test", test);
+
+document.querySelector(".campos").innerHTML = test.join("")
+
+
+{/* <div class="form-group">
+    <label for="nombre">nombre</label>
+    <input class="form-control" type="text" name="nombre" id="nombre" placeholder="Ej: Juan">
+</div>
+ */}
+
 const botonEnviar = document.querySelector(".enviar")
 botonEnviar.addEventListener("click", function (params) {
     params.preventDefault();
